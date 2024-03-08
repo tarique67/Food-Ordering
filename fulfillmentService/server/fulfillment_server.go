46233cefed3b6d1f9596907583731b3525f9febe
tarique67 <tarique.anwar2_ftc@external.swiggy.in>
2024-03-09 01:36:12 +0530

    implemented assign order and update delivery status

--- fulfillmentService/server/fulfillment_server.go
+++ fulfillmentService/server/fulfillment_server.go
@@ -11,8 +11,11 @@ import (
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func init() {
@@ -22,25 +25,25 @@ func init() {
 type Status string
 
 const (
-	ACCEPTED Status = "ACCEPTED"
-	ASSIGNED Status = "ASSIGNED"
+	ACCEPTED  Status = "ACCEPTED"
+	ASSIGNED  Status = "ASSIGNED"
 	PICKED_UP Status = "PICKED_UP"
 	DELIVERED Status = "DELIVERED"
 )
 
-type Order struct{
-	gorm.Model
-	OrderID int64 `gorm:"primarykey"`
-	DeliveryStatus Status 
+type Order struct {
+	OrderID        int64  `gorm:"primarykey"`
+	DeliveryStatus Status `gorm:"type:varchar(100)"`
+	DeliveryExecutiveId int64
 }
 
 type Delivery_Executive struct {
-	gorm.Model
-	ID        string `gorm:"primarykey"`
-	Name      string
-	Location  string
-	OrderIds  []Order `gorm:"foreignkey:OrderID"`
-	Token     string
+	ID       int64 `gorm:"primarykey AUTO_INCREMENT"`
+	Name     string
+	Location string
+	OrderIds []Order `gorm:"foreignkey:DeliveryExecutiveId"`
+	Availability bool 
+	Token    string
 }
 
 func DatabaseConnection() *gorm.DB {
@@ -56,7 +59,11 @@ func DatabaseConnection() *gorm.DB {
 		dbName,
 		password,
 	)
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	DB.Exec("DROP TABLE IF EXISTS orders;")
+	DB.Exec("DROP TABLE IF EXISTS delivery_executives;")
 	DB.AutoMigrate(Delivery_Executive{}, Order{})
 	if err != nil {
 		log.Fatal("Error connecting to the database...", err)
@@ -77,15 +84,14 @@ type server struct {
 func (s *server) CreateDeliveryExecutive(ctx context.Context, req *pb.CreateDeliveryExecutiveReuqest) (*pb.CreateDeliveryExecutiveResponse, error) {
 	fmt.Println("Create Delivery Executive.")
 	delivery_executive := req.GetDeliveryExecutive()
-	delivery_executive.DeliveryExecutiveId = uuid.New().String()
 	delivery_executive.Token = uuid.New().String()
 
 	data := Delivery_Executive{
-		ID:    delivery_executive.GetDeliveryExecutiveId(),
-		Name:  delivery_executive.GetName(),
+		Name:     delivery_executive.GetName(),
 		Location: delivery_executive.GetLocation(),
 		OrderIds: []Order{},
-		Token: delivery_executive.GetToken(),
+		Availability: true,
+		Token:    delivery_executive.GetToken(),
 	}
 
 	res := s.DB.Create(&data)
@@ -95,13 +101,83 @@ func (s *server) CreateDeliveryExecutive(ctx context.Context, req *pb.CreateDeli
 
 	return &pb.CreateDeliveryExecutiveResponse{
 		DeliveryExecutive: &pb.Deliver_Executive{
-			DeliveryExecutiveId:    data.ID,
-			Name:  data.Name,
-			Location: data.Location,
-			DeliveryOrders: []*pb.Delivery_Order{},
-			Token: data.Token,
+			DeliveryExecutiveId: data.ID,
+			Name:                data.Name,
+			Location:            data.Location,
+			DeliveryOrders:      []*pb.Delivery_Order{},
+			Token:               data.Token,
 		},
 	}, nil
+}
+
+func (s *server) AssignOrder(ctx context.Context, req *pb.AssignOrderRequest) (*pb.AssignOrderResponse, error) {
+	fmt.Println("Assign Order to Delivery Executive.")
+	order := req.GetOrder()
+
+	var deliveryExecutive Delivery_Executive
+	if err := s.DB.Where("location = ?  And availability = ?", order.PickUpLocation, true).First(&deliveryExecutive).Error; err != nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			"could not find delivery executive with location %s: %v",
+			order.PickUpLocation,
+			err,
+		)
+	}
+
+	assignedOrder := Order{
+		OrderID:        order.GetOrderId(),
+		DeliveryStatus: ASSIGNED,
+	}
+
+	deliveryExecutive.OrderIds = append(deliveryExecutive.OrderIds, assignedOrder)
+	deliveryExecutive.Availability = false
+
+	if err := s.DB.Save(&deliveryExecutive).Error; err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"failed to update delivery executive: %v",
+			err,
+		)
+	}
+
+	order.Status = string(ASSIGNED)
+	order.DeliveryExecutiveId = deliveryExecutive.ID
+
+	return &pb.AssignOrderResponse{
+		Order: order,
+	}, nil
+}
+
+func (s *server) UpdateStatus(ctx context.Context, req *pb.UpdateStatusRequest) (*pb.UpdateStatusResponse, error) {
+	fmt.Println("Update Status")
+    orderId := req.GetOrderId()
+    status := req.GetStatus()
+
+    var order Order
+    if err := s.DB.Where("order_id = ?", orderId).First(&order).Error; err != nil {
+        return nil, err
+    }
+
+    if err := s.DB.Model(&order).Update("delivery_status", status).Error; err != nil {
+        return nil, err
+    }
+
+    if status == "DELIVERED" {
+        var deliveryExecutive Delivery_Executive
+        if err := s.DB.Where("id = ?", order.DeliveryExecutiveId).First(&deliveryExecutive).Error; err != nil {
+            return nil, err
+        }
+
+        deliveryExecutive.Availability = true
+        if err := s.DB.Save(&deliveryExecutive).Error; err != nil {
+            return nil, err
+        }
+    }
+
+    return &pb.UpdateStatusResponse{
+        OrderId: orderId,
+        Status:  status,
+    }, nil
 }
 
 func main() {
